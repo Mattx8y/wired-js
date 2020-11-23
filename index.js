9d5fe4f7e01aa0cc1835ded0f51cea0e6d0bd2d5
iWillBanU <wrscr.surpertop@gmail.com>
2020-11-23 11:25:04 -0500

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  Client: require("./src/Client.js"),
-  GatewayConnection: require("./src/gateway/GatewayConnection.js"),
-  Intents: require("./src/gateway/Intents.js"),
-  RESTManager: require("./src/rest/RESTManager.js"),
-  ShardManager: require("./src/shards/ShardManager.js")
-}
